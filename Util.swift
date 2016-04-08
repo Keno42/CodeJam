fd7b017ee4c91360e2285463bd0353e04a9e0ded
Kenichi Ueno <kenichi.ueno@linecorp.com>
2016-04-08 16:28:00 +0900

    add permutations

--- Util.swift
+++ Util.swift
@@ -1,4 +1,5 @@
-extension String: CollectionType {} // for String.split()
+// http://qiita.com/y_mazun/items/dc2a0cad8da1c0e88a40
+extension String: CollectionType {} // to enable String.split()
 
 // functions
 func readInt() -> Int { return readLine().flatMap { Int($0) }! }
@@ -8,3 +9,12 @@ func readDoubles() -> [Double] { return readLine().flatMap { $0.split(" ").flatM
 func readString() -> String { return readLine()! }
 func readStrings() -> [String] { return readLine().flatMap { $0.split(" ") }! }
 
+// http://stackoverflow.com/questions/34968470/calculate-all-permutations-of-a-string-in-swift
+func permutations(n:Int, inout _ a:Array<Character>) {
+    if n == 1 {print(a); return}
+    for i in 0..<n-1 {
+        permutations(n-1,&a)
+        swap(&a[n-1], &a[(n%2 == 1) ? 0 : i])
+    }
+    permutations(n-1,&a)
+}
